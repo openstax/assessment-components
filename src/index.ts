65851a19f69ba2f7524de4660dfb46e790c2f93f
Josiah Ivey <josiah.ivey@gmail.com>
2022-08-05 10:41:31 -0700

    export card

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './components/Answer';
 export * from './components/AnswersTable';
 export * from './components/Question';
 export * from './components/FreeResponseInput';
+export * from './components/Card';
