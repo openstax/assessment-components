47d9fd9eae0910cc0384d8a90a83730260c72eaf
Beth Shook <es66@rice.edu>
2022-08-08 13:57:46 -0500

    incorporate exercise question components

--- src/components/FreeResponseInput.tsx
+++ src/components/FreeResponseInput.tsx
@@ -1,10 +1,11 @@
 import { MouseEventHandler, ReactNode } from 'react';
 import { countWords } from '../utils';
 import styled, { css } from 'styled-components';
-import { breakpoints, colors } from '../theme';
+import { colors } from '../theme';
 import { AvailablePoints, Question as QuestionType } from 'src/types';
 import { QuestionHtml } from './Question';
 import Button from './Button';
+import { StepCardFooter } from './StepCardFooter';
 
 export interface FreeResponseProps {
   readOnly: boolean;
@@ -82,62 +83,7 @@ const TextArea = styled.textarea<FreeResponseProps>`
 `;
 TextArea.displayName = 'TextArea';
 
-export const StepCardFooter = styled.div`
-    padding: var(--step-card-padding-top) var(--step-card-padding-side);
-    border-top: 1px solid ${colors.palette.pale};
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    font-size: 1.4rem;
-    line-height: 2rem;
-
-    > * {
-        flex-grow: 1;
-    }
-
-    button {
-        width: 160px;
-        height: 48px;
-    }
-
-    .points {
-        margin-bottom: 1.6rem; // Replace with https://caniuse.com/?search=gap soon
-
-        .attempts-left {
-            color: #F36B32;
-        }
-    }
-
-    .controls {
-        display: flex;
-        flex-flow: column wrap-reverse;
-        margin-left: 1.6rem; // Replace with https://caniuse.com/?search=gap soon
-
-        button + button {
-            margin: 0.8rem 0 0 0;
-        }
-    }
-
-    ${breakpoints.desktop`
-        padding: 32px var(--step-card-padding-side);
-        flex-wrap: nowrap;
-
-        .points {
-            max-width: 400px;
-        }
-
-        .controls {
-            flex-flow: row;
-            justify-content: flex-end;
-
-            button + button {
-                margin: 0 0 0 0.8rem;
-            }
-        }
-    `}
-`;
-
-const ButtonLg = styled(Button)`
+const SubmitBtn = styled(Button)`
   align-self: flex-end;
   margin: 4rem;
   float: right;
@@ -199,13 +145,13 @@ export const FreeResponseInput = (props: FreeResponseProps) => {
           >
             Cancel
           </Button>
-          <ButtonLg
+          <SubmitBtn
               data-test-id="submit-answer-btn"
               disabled={isSubmitDisabled}
               onClick={saveHandler}
           >
               {submitBtnLabel}
-          </ButtonLg>
+          </SubmitBtn>
         </div>
       </StepCardFooter>
     </StyledFreeResponse>
