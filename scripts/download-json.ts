2ee2afaf4f055799bb8781450f2a54de93c87b3e
Dante Soares <dante.m.soares@rice.edu>
2022-08-16 13:59:15 -0500

    Added token option to fetch exercises with secret solutions

--- scripts/download-json.ts
+++ scripts/download-json.ts
@@ -5,19 +5,31 @@ import fetch from 'node-fetch';
   // 100 is the max the API will accept
   const perPage = 100;
 
-  let filename = 'exercises.json';
-  const outputArgsIndex = process.argv.indexOf('-o');
-  if (outputArgsIndex !== -1) {
-    const outputArgs = process.argv.splice(outputArgsIndex, 2);
-    filename = outputArgs[1];
-  }
+  const spliceOption = ((flag: string, defaultValue?: string) => {
+    const argIndex = process.argv.indexOf(flag);
+    if (argIndex === -1) {
+      return defaultValue;
+    } else {
+      return process.argv.splice(argIndex, 2)[1];
+    }
+  }) as ((flag: string, defaultValue: string) => string) & ((flag: string) => string | undefined)
+
+  const filename = spliceOption('-o', 'exercises.json');
+  const token = spliceOption('-t');
 
   if (process.argv.length < 3) {
-    console.error(`Usage: ${process.argv[0]} ${process.argv[1]} [-o exercises.json] <query>`);
+    console.error(
+      `Usage: ${process.argv[0]} ${process.argv[1]} [-o exercises.json] [-t token] <query>`
+    );
     process.exit(1);
   }
 
   const query = process.argv.slice(2).join(' ');
+
+  const headers = {};
+  if (token !== undefined) {
+    headers['Authorization'] = `Bearer ${token}`;
+  }
 
   const searchApiUrl = (query: string, page: number) => (
     `https://exercises.openstax.org/api/exercises?per_page=${perPage}&page=${page}&query=${query}`
@@ -26,7 +38,7 @@ import fetch from 'node-fetch';
   let totalPages = 1;
   const exercises: object[] = [];
   for (let page = 1; page <= totalPages; page++) {
-    const response = await fetch(searchApiUrl(query, page));
+    const response = await fetch(searchApiUrl(query, page), { headers });
     const { total_count, items } = await response.json();
 
     totalPages = Math.ceil(total_count/perPage);
