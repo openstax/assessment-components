c9d2028056d6866af5a96f38b7cbe2872b7ceff4
Beth Shook <es66@rice.edu>
2022-09-12 07:38:40 -0500

    lint fix

--- src/components/ProgressBar.stories.tsx
+++ src/components/ProgressBar.stories.tsx
@@ -8,6 +8,6 @@ const props: ProgressBarProps = {
   steps: variants.map((variant) => ({variant})),
 }
 
-export const Default = () => <ProgressBar {...props} steps={props.steps.map((step) => step = {variant: null})} />;
+export const Default = () => <ProgressBar {...props} steps={props.steps.map((step) => Object.assign({}, step, {variant: null}))} />;
 export const InProgress = () => <ProgressBar {...props} activeIndex={4} />;
 export const NavigatedBack = () => <ProgressBar {...props} activeIndex={2} />;
