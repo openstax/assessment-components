a1e499dc6207dda092b108da2fed8a0f111806bc
JD Ivey <josiah.ivey@gmail.com>
2022-09-13 11:12:02 -0700

    Integration fixes (#18)
    * fix declarations not being found when importing package
    
    * fix module type
    
    * make react-dom a peer dependency
    
    * make react types match react versions
    
    * update render version to match, fix warning
    
    * fix dependency
    
    * update snapshots from test renderer upgrade
    
    * fix div inside span, fix rendering empty unneeded markup
    
    * add workaround to extension issue for now
    
    * fix typo
    
    * revert markup change for now
    
    * fix snapshots

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './components/AnswersTable';
 export * from './components/Question';
 export * from './components/FreeResponseInput';
 export * from './components/Card';
+export * from './components/Exercise';
