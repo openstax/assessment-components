78216b7e99108e3b2dd8cf44dcb67328711fdbcc
Josiah Ivey <josiah.ivey@gmail.com>
2022-09-16 08:18:01 -0700

    dist

---
+++ dist/src/components/ProgressBar.d.ts
@@ -0,0 +1,25 @@
+export declare const StyledItem: import("styled-components").StyledComponent<"button", any, {
+    variant: ProgressBarItemVariant;
+    isActive: boolean;
+}, never>;
+export interface ProgressBarProps<S extends {
+    variant: ProgressBarItemVariant;
+}> {
+    steps: S[];
+    activeIndex: number | null;
+    goToStep: (index: number, step: S) => void;
+}
+export interface ProgressBarItemProps<S> {
+    index: number;
+    isActive: boolean;
+    step: S;
+    goToStep: (index: number, step: S) => void;
+}
+export declare type ProgressBarItemVariant = 'isCorrect' | 'isIncorrect' | null;
+export declare const ProgressBarItem: <S extends {
+    variant: ProgressBarItemVariant;
+}>({ index, isActive, step, goToStep }: ProgressBarItemProps<S>) => JSX.Element;
+declare const ProgressBar: <S extends {
+    variant: ProgressBarItemVariant;
+}>({ steps, activeIndex, goToStep }: ProgressBarProps<S>) => JSX.Element;
+export default ProgressBar;
