e12fcecaf87b7a39161db1270913c13c3c1b63e1
Josiah Ivey <josiah.ivey@gmail.com>
2022-09-15 21:12:34 -0700

    dist

---
+++ dist/src/components/Exercise.d.ts
@@ -0,0 +1,25 @@
+import { ExerciseData, ID, QuestionState, StepBase, StepWithData } from '../../src/types';
+interface ExerciseBaseProps {
+    step: StepBase;
+    exercise: ExerciseData;
+    numberOfQuestions: number;
+    questionNumber: number;
+    canAnswer: boolean;
+    needsSaved: boolean;
+    answer_id_order?: ID[];
+    hasMultipleAttempts: boolean;
+    onAnswerChange: () => void;
+    onAnswerSave: () => void;
+    onNextStep: () => void;
+    apiIsPending: boolean;
+}
+export interface ExerciseWithStepDataProps extends ExerciseBaseProps {
+    step: StepWithData;
+}
+export interface ExerciseWithQuestionStatesProps extends ExerciseBaseProps {
+    questionStates: {
+        [key: ID]: QuestionState;
+    };
+}
+export declare const Exercise: ({ numberOfQuestions, questionNumber, step, exercise, canAnswer, needsSaved, ...props }: ExerciseWithStepDataProps | ExerciseWithQuestionStatesProps) => JSX.Element;
+export {};
