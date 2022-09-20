f1a5fe62d20e92e2c4c9cf0987cc3db426fb7619
Josiah Ivey <josiah.ivey@gmail.com>
2022-09-19 20:45:36 -0700

    dist

---
+++ dist/src/components/Card.d.ts
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+import { AvailablePoints, StepBase, StepWithData } from "../types";
+export declare const InnerStepCard: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const OuterStepCard: import("styled-components").StyledComponent<"div", any, {}, never>;
+interface SharedProps {
+    questionNumber: number;
+    numberOfQuestions: number;
+    leftHeaderChildren?: ReactNode;
+    rightHeaderChildren?: ReactNode;
+    headerTitleChildren?: ReactNode;
+}
+export interface StepCardProps extends SharedProps {
+    unpadded: boolean;
+    className?: string;
+    children?: ReactNode;
+    stepType: StepWithData['type'];
+    availablePoints?: AvailablePoints;
+    questionId?: string;
+    multipartBadge?: ReactNode;
+    isHomework: boolean;
+    wrapsExercise: boolean;
+}
+declare const StepCard: {
+    ({ questionNumber, numberOfQuestions, stepType, isHomework, availablePoints, unpadded, className, children, questionId, multipartBadge, leftHeaderChildren, rightHeaderChildren, headerTitleChildren, wrapsExercise, ...otherProps }: StepCardProps): JSX.Element;
+    displayName: string;
+};
+export interface TaskStepCardProps extends SharedProps {
+    className?: string;
+    children?: ReactNode;
+    step: StepBase | StepWithData;
+    questionNumber: number;
+    numberOfQuestions: number;
+    wrapsExercise: boolean;
+}
+declare const TaskStepCard: {
+    ({ step, questionNumber, numberOfQuestions, children, className, ...otherProps }: TaskStepCardProps): JSX.Element;
+    displayName: string;
+};
+export { StepCard, TaskStepCard };
