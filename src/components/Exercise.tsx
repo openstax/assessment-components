70598c8af2deb74dae4953536237181422fc94ef
Beth Shook <es66@rice.edu>
2022-09-22 19:56:20 -0500

    add scrollToElement

--- src/components/Exercise.tsx
+++ src/components/Exercise.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import styled from 'styled-components';
 import { Content } from './Content';
 import { TaskStepCard } from './Card';
 import { Answer, ExerciseData, ID, QuestionState, StepBase, StepWithData } from '../../src/types';
 import { ExerciseQuestion } from './ExerciseQuestion';
+import scrollToElement from 'scroll-to-element';
 
 const StyledTaskStepCard = styled(TaskStepCard)`
   font-size: 1.8rem;
@@ -51,9 +53,16 @@ export interface ExerciseWithQuestionStatesProps extends ExerciseBaseProps {
 }
 
 export const Exercise = ({
-  numberOfQuestions, questionNumber, step, exercise, show_all_feedback, ...props
+  numberOfQuestions, questionNumber, step, exercise, show_all_feedback, scrollToQuestion, ...props
 }: ExerciseWithStepDataProps | ExerciseWithQuestionStatesProps) => {
   const legacyStepRender = 'feedback_html' in step;
+
+  React.useEffect(() => {
+    const el = document.querySelector(`.openstax-question[data-question-number="${scrollToQuestion}"]`)
+    if (el) {
+      scrollToElement(el);
+    }
+  }, [scrollToQuestion]);
 
   return (<StyledTaskStepCard
     step={step}
