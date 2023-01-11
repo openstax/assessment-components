1e3b833ced5c50254680613b9940c7b635937487
Beth Shook <beth.shook@gmail.com>
2023-01-11 15:27:14 -0600

    Completion status screen (#47)
    * build out new component
    
    * add tets
    
    * update one variant
    
    * rename ladle story
    
    Co-authored-by: Beth Shook <es66@rice.edu>

---
+++ src/components/CompletionStatus.stories.tsx
@@ -0,0 +1,14 @@
+import { CompletionStatus, CompletionStatusProps } from './CompletionStatus';
+
+const props: CompletionStatusProps = {
+  numberOfQuestions: 15,
+  numberCompleted: 0,
+  canContinue: false,
+  handleClick: () => {console.log('click')}
+};
+
+export const Default = () => <CompletionStatus {...props} />;
+export const PartialComplete = () => <CompletionStatus {...props} numberCompleted={3} />
+export const CompleteWithContinue = () => <CompletionStatus {...props} numberCompleted={15} canContinue={true} />
+export const Complete = () => <CompletionStatus {...props} numberCompleted={15} canContinue={false} />
+
