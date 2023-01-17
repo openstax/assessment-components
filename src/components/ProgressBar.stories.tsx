6d754d50ca88b9919ecf5a4d0fc8f5321974c0a4
Beth Shook <es66@rice.edu>
2023-01-17 11:42:33 -0600

    add flag icon to progress bar

--- src/components/ProgressBar.stories.tsx
+++ src/components/ProgressBar.stories.tsx
@@ -1,13 +1,15 @@
 import { ProgressBar, ProgressBarItemVariant, ProgressBarProps } from './ProgressBar';
 
-const variants: ProgressBarItemVariant[] = ['isIncorrect', 'isCorrect', 'isIncorrect', 'isIncorrect', null, null, null, null];
+const variants = new Array(8).fill({variant: null});
+const variantsInProgress: ProgressBarItemVariant[] = ['isIncorrect', 'isCorrect', 'isIncorrect', 'isIncorrect', null, null, null, null, 'isStatus'];
 
 const props: ProgressBarProps<{variant: ProgressBarItemVariant}> = {
   activeIndex: 0,
   goToStep: (index: number, step) => console.log(index, step),
-  steps: variants.map((variant) => ({variant})),
+  steps: variantsInProgress.map((variant) => ({variant})),
 }
 
-export const Default = () => <ProgressBar {...props} steps={props.steps.map((step) => Object.assign({}, step, {variant: null}))} />;
+export const Default = () => <ProgressBar {...props} steps={[...variants, {variant: 'isStatus'}]} />;
 export const InProgress = () => <ProgressBar {...props} activeIndex={4} />;
 export const Review = () => <ProgressBar {...props} activeIndex={2} />;
+export const StatusStep = () => <ProgressBar {...props} activeIndex={8} />;
