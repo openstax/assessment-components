dd61611ecca8fb6f9edc21d8f2c60015a2bfb08e
Josiah Ivey <josiah.ivey@gmail.com>
2023-06-07 14:06:08 -0700

    add script to get EOC tagged exercises and create PDFs from orn url
    usage:
    
    yarn generate-eoc-pdfs -t [TOKEN] [ORN_BOOK_URL]
    
    adding --debug will display the output of the child processes which
    may be helpful if you run into issues.

--- scripts/download-json.ts
+++ scripts/download-json.ts
@@ -36,6 +36,12 @@ import fetch from 'node-fetch';
   const exercises: object[] = [];
   for (let page = 1; page <= totalPages; page++) {
     const response = await fetch(searchApiUrl(query, page), { headers });
+    console.log(response.url);
+    if (response.status !== 200) {
+      console.error(`Error: Exercises responded with ${response.status}: ${response.statusText} for ${response.url}`);
+      process.exit(1);
+    }
+
     const { total_count, items } = await response.json();
 
     totalPages = Math.ceil(total_count/perPage);
