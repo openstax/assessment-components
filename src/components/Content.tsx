a9270f595505d20d1cb448d2c9472981ae192c92
Josiah Ivey <josiah.ivey@gmail.com>
2023-06-30 08:47:57 -0700

    fix mathjax missing preamble by rendering all Content components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { typesetMath } from "../helpers/mathjax";
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 type ComponentType = keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>;
@@ -15,12 +16,20 @@ export interface ContentProps<T extends ComponentType | undefined> {
 export const Content = (<T extends ComponentType | undefined>(
   {html, component, block = false, ...props}: ContentProps<T>
 ) => {
+  const container = React.useRef(null);
+
+  React.useEffect(() => {
+    if (container.current) {
+      typesetMath(container.current);
+    }
+  }, [html]);
+
   if (component !== undefined) {
-    return React.cloneElement(component, {html, ...props});
+    return React.cloneElement(component, {html, ref: container, ...props});
   }
   if (block) {
-    return <div dangerouslySetInnerHTML={{ __html: html }} {...props} />;
+    return <div dangerouslySetInnerHTML={{ __html: html }} ref={container} {...props} />;
   } else {
-    return <span dangerouslySetInnerHTML={{ __html: html }} {...props} />;
+    return <span dangerouslySetInnerHTML={{ __html: html }} ref={container} {...props} />;
   }
 });
