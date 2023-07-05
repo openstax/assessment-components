eadc54f6c0aab88655c814890df9a62f7ab394ab
Josiah Ivey <josiah.ivey@gmail.com>
2023-07-05 14:07:39 -0700

    Update src/hooks/useTypesetMath.ts
    Co-authored-by: Thomas Woodward <tom.g.woodward@gmail.com>

--- src/hooks/useTypesetMath.ts
+++ src/hooks/useTypesetMath.ts
@@ -1,4 +1,4 @@
 import React from "react";
 
-export const TypesetMathContext = React.createContext<{ typesetMath?: () => void }>({});
+export const TypesetMathContext = React.createContext<() => void>(() => { throw new Error('context not initialized'))});
 export const useTypesetMath = () => React.useContext(TypesetMathContext);
