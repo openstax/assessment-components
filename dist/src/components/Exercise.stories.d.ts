6f5fc4e2f9f540950894abd969cbe7710ded8185
Tyler Nullmeier <tylerzeromaster@gmail.com>
2024-10-25 12:46:48 -0500

    Continue when completed
    _____

--- dist/src/components/Exercise.stories.d.ts
+++ dist/src/components/Exercise.stories.d.ts
@@ -1,6 +1,8 @@
 export declare const Default: () => JSX.Element;
+export declare const DefaultWithoutFeedback: () => JSX.Element;
 export declare const DeprecatedStepData: () => JSX.Element;
 export declare const CompleteWithFeedback: () => JSX.Element;
+export declare const CompleteWithoutFeedback: () => JSX.Element;
 export declare const IncorrectWithFeedbackAndSolution: () => JSX.Element;
 export declare const IncorrectWithFeedbackAndSolutionWrappingText: () => JSX.Element;
 export declare const MultiPartHalfComplete: () => JSX.Element;
