add7bcb15044895173972f3eeb6f8b7c87534c22
Tyler Nullmeier <tylerzeromaster@gmail.com>
2024-12-10 15:41:00 -0600

    dist

---
+++ dist/src/components/Answer.d.ts
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+import { Answer as AnswerType, ID } from '../types';
+export interface AnswerProps {
+    answer: AnswerType;
+    iter: number;
+    qid: ID;
+    type: 'teacher-review' | 'teacher-preview' | 'student' | 'student-mpp';
+    hasCorrectAnswer?: boolean;
+    onChangeAnswer?: (answer: AnswerType) => void;
+    disabled: boolean;
+    answerId?: ID;
+    correctAnswerId?: ID | null;
+    incorrectAnswerId?: ID;
+    onKeyPress?: () => void;
+    answered_count?: number;
+    correctIncorrectIcon?: ReactNode;
+    radioBox?: ReactNode;
+    contentRenderer?: JSX.Element;
+    show_all_feedback?: boolean;
+    tableFeedbackEnabled?: boolean;
+    feedbackId?: string;
+}
+export declare const Answer: {
+    (props: AnswerProps): JSX.Element;
+    displayName: string;
+};
