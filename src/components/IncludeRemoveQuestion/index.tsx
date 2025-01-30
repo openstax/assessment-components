42d25f7d546699f7bb660cb8187494d45fff3b1e
jomcarvajal <jose.carvajal@edify.cr>
2025-01-30 08:50:13 -0600

    Add prop method for details button

--- src/components/IncludeRemoveQuestion/index.tsx
+++ src/components/IncludeRemoveQuestion/index.tsx
@@ -7,40 +7,43 @@ export interface IncludeRemoveQuestionProps {
   buttonVariant: 'include' | 'remove';
   // Method invoked when the include button is clicked
   onIncludeHandler: () => void;
-    // Method invoked when the remove button is clicked
+  // Method invoked when the remove button is clicked
   onRemoveHandler: () => void;
+  // Method invoked when the details button is pressed
+  onClickDetails?: () => void;
 }
 
-export const IncludeRemoveQuestion = ({buttonVariant, onIncludeHandler, onRemoveHandler }: IncludeRemoveQuestionProps) => {
+export const IncludeRemoveQuestion = (
+  { buttonVariant, onIncludeHandler, onRemoveHandler, onClickDetails }: IncludeRemoveQuestionProps) => {
 
-    const buttonIcon = React.useMemo(() => buttonVariant === 'include' ? faPlus : faMinus, [buttonVariant]);
-    const onClickHandler = (variant: 'include' | 'remove') => { 
-        switch (variant) {
-            case 'include':
-              onIncludeHandler();
-              break;
-            case 'remove':
-              onRemoveHandler();
-              break;
-            default:
-              break;
-          }
-    };
+  const buttonIcon = React.useMemo(() => buttonVariant === 'include' ? faPlus : faMinus, [buttonVariant]);
+  const onClickHandler = (variant: 'include' | 'remove') => {
+    switch (variant) {
+      case 'include':
+        onIncludeHandler();
+        break;
+      case 'remove':
+        onRemoveHandler();
+        break;
+      default:
+        break;
+    }
+  };
 
-    const generateButtonText = (string: string) => {
-      return string.charAt(0).toUpperCase() + string.slice(1) + ' question';
-    };
-    
-    return (
-        <StyledContainer>
-            <StyledButton className={buttonVariant} onClick={() => onClickHandler(buttonVariant)} aria-label={buttonVariant}>
-                <StyledIcon className={buttonVariant} icon={buttonIcon} aria-label={buttonVariant + ' question'} border size="lg" />
-                <span>{generateButtonText(buttonVariant)}</span>
-            </StyledButton>
-            <StyledButton className="details" aria-label="details">
-                <StyledIcon className="details" icon={faEllipsisH} border size="lg"/>
-                <span>Details</span>
-            </StyledButton>
-        </StyledContainer>
-    );
+  const generateButtonText = (string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1) + ' question';
+  };
+
+  return (
+    <StyledContainer>
+      <StyledButton className={buttonVariant} onClick={() => onClickHandler(buttonVariant)} aria-label={buttonVariant}>
+        <StyledIcon className={buttonVariant} icon={buttonIcon} aria-label={buttonVariant + ' question'} border size="lg" />
+        <span>{generateButtonText(buttonVariant)}</span>
+      </StyledButton>
+      <StyledButton className="details" aria-label="details" onClick={onClickDetails}>
+        <StyledIcon className="details" icon={faEllipsisH} border size="lg" />
+        <span>Details</span>
+      </StyledButton>
+    </StyledContainer>
+  );
 }; 
