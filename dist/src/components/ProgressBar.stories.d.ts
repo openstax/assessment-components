a139d1ceaafbd8ba4de0ec41cd05b9a594872fea
jomcarvajal <jose.carvajal@edify.cr>
2025-06-05 11:39:50 -0600

    dist

---
+++ dist/src/components/ProgressBar.stories.d.ts
@@ -0,0 +1,5 @@
+export declare const Default: () => JSX.Element;
+export declare const InProgress: () => JSX.Element;
+export declare const Review: () => JSX.Element;
+export declare const StatusStep: () => JSX.Element;
+export declare const InProgressNoFeedback: () => JSX.Element;
