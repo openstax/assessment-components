dfa647c95087386c3f75a472c73bd27b36226fe8
Prabhdip Gill <pg53@rice.edu>
2025-06-17 00:39:29 -0700

    Add media modal and manager

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useTypesetMath } from "../hooks/useTypesetMath";
+import { createMediaModalManager } from "./modalManager";
 
-/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 type ComponentType = keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>;
 
 export interface ContentProps<T extends ComponentType | undefined> {
@@ -13,21 +13,49 @@ export interface ContentProps<T extends ComponentType | undefined> {
   block?: boolean;
 }
 
+function enhanceImagesForAccessibility(rootEl: HTMLElement) {
+  rootEl.querySelectorAll('img').forEach((img) => {
+    img.setAttribute('tabindex', '0');
+    img.setAttribute('role', 'button');
+    img.setAttribute('aria-label', 'Open media preview');
+  });
+}
+
 export const Content = (<T extends ComponentType | undefined>(
-  {html, component, block = false, ...props}: ContentProps<T>
+  { html, component, block = false, ...props }: ContentProps<T>
 ) => {
   const typesetMath = useTypesetMath();
+  const divRef = useRef<HTMLDivElement>(null);
+  const spanRef = useRef<HTMLSpanElement>(null);
+  const [MediaModalPortal, setMediaModalPortal] = useState<() => JSX.Element | null>(() => () => null);
 
-  React.useEffect(() => {
+  useEffect(() => {
+    const container = block ? divRef.current : spanRef.current;
+    if (!container) return;
+
     typesetMath();
-  }, [typesetMath, html])
+    enhanceImagesForAccessibility(container);
+
+    const mediaModalManager = createMediaModalManager(container);
+    setMediaModalPortal(() => mediaModalManager.MediaModalPortal);
+
+    return () => {
+      mediaModalManager.detachListeners();
+    };
+  }, [typesetMath, html, block]);
 
   if (component !== undefined) {
-    return React.cloneElement(component, {html, ...props});
+    return React.cloneElement(component, { html, ...props });
   }
-  if (block) {
-    return <div dangerouslySetInnerHTML={{ __html: html }} {...props} />;
-  } else {
-    return <span dangerouslySetInnerHTML={{ __html: html }} {...props} />;
-  }
+
+  return (
+    <>
+      {block ? (
+        <div ref={divRef} dangerouslySetInnerHTML={{ __html: html }} {...props} />
+      ) : (
+        <span ref={spanRef} dangerouslySetInnerHTML={{ __html: html }} {...props} />
+      )}
+      <MediaModalPortal />
+    </>
+  );
 });
