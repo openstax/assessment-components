0780b3842e771ada773b61e68c33c3157673cf1a
Prabhdip Gill <pg53@rice.edu>
2025-09-22 01:15:00 -0700

    Update completion status with unlimited attempts

--- src/components/CompletionStatus.stories.tsx
+++ src/components/CompletionStatus.stories.tsx
@@ -6,7 +6,18 @@ const props: CompletionStatusProps = {
   handleClick: () => {console.log('click')}
 };
 
+const unlimitedProps: CompletionStatusProps = {
+  numberOfQuestions: 15,
+  numberCompleted: 0,
+  handleClick: () => {console.log('click')},
+  handleRetry: () => {console.log('click')},
+  scoreSoFar: '5/10',
+  savedScore: '7/10'
+} 
 export const Default = () => <CompletionStatus {...props} />;
 export const PartialComplete = () => <CompletionStatus {...props} numberCompleted={3} />
 export const Complete = () => <CompletionStatus {...props} numberCompleted={15} />
+export const unlimitedComplete = () => <CompletionStatus {...unlimitedProps} numberCompleted={15}/>;
+export const unlimitedPartial = () => <CompletionStatus {...unlimitedProps} numberCompleted={3} handleRetry={() => {console.log('Banana')}} />
+export const unlimitedCompletedNoScore = () => <CompletionStatus {...unlimitedProps} scoreSoFar={undefined} savedScore={undefined} numberCompleted={15}/>;
 
--- src/components/CompletionStatus.tsx
+++ src/components/CompletionStatus.tsx
@@ -13,6 +13,9 @@ export interface CompletionStatusProps {
   numberCompleted: number;
   handleClick: () => void;
   className?: string;
+  scoreSoFar?: string;
+  savedScore?: string;
+  handleRetry?: () => void;
 }
 
 const CompletionStatusCard = styled(InnerStepCard)`
@@ -36,24 +39,114 @@ const CompletionHeader = styled.h2`
   margin: 0;
 `;
 
-export const CompletionStatus = styled(({
-  numberOfQuestions, numberCompleted, handleClick, className
-}: CompletionStatusProps) => {
+const ButtonGroup = styled.div`
+  display: flex;
+  margin: 0;
+  gap: 1rem;
 
+  button {
+    min-width: 100px;
+    height: 48px;
+  }
+`;
+
+const ScoreGroup = styled.div`
+  display: flex;
+  margin: 0;
+  gap: 1rem;
+`;
+
+const RetryResumeButton = styled(Button)`
+  background-color: #ffffff;
+  color: #000000;
+  border: 1px solid #D5D5D5;
+  font-weight: normal;
+  transition: background-color 0.2s ease;
+
+  &:hover {
+    background-color: #f5f5f5 !important;
+    color: #000000 !important;
+    border: 1px solid #D5D5D5 !important;
+  }
+
+  &:active {
+    background-color: #eaeaea !important;
+    color: #000000 !important;
+    border: 1px solid #D5D5D5 !important;
+  }
+`;
+
+export const CompletionStatus = styled(({
+  numberOfQuestions,
+  numberCompleted,
+  handleClick,
+  className,
+  scoreSoFar,
+  savedScore,
+  handleRetry
+}: CompletionStatusProps) => {
   const allCompleted = numberOfQuestions === numberCompleted;
   const someCompleted = numberCompleted > 0;
-  const buttonText = allCompleted ? 'Next' : (
-      someCompleted ? 'Continue' : 'Start'
-  );
 
-  return <>
-    <GlobalStyle />
-    <CompletionStatusCard className={className}>
-      <CompletionHeader>{allCompleted ? 'You are done.' : (someCompleted ? 'Quiz is partially complete.' : 'No questions have been answered.')}</CompletionHeader>
-      <p>{allCompleted ? 'Great job answering all the questions.' : (someCompleted ? `You've completed ${numberCompleted} of ${numberOfQuestions} questions.` : 'Begin working on the quiz.')}</p>
-      <Button data-test-id={`${buttonText.split(' ')[0].toLowerCase()}-btn`} onClick={() => handleClick()}>
-        {buttonText}
-      </Button>
-    </CompletionStatusCard>
-  </>
+  const buttonText = allCompleted ? 'Next' : (someCompleted ? 'Continue' : 'Start');
+
+  const retryOrResume = allCompleted ? 'Retry Quiz' : 'Resume Quiz';
+  const unlimitedDone = "Attempts for this quiz are unlimited. Your highest score will be saved.";
+  const unlimitetedCurrent = "You are in the middle of a quiz attempt. Attempts for this quiz are unlimited. Your highest score will be saved.";
+
+  // When allCompleted, clicking Retry/Resume should create a new attempt (handleRetry)
+  // When not completed, clicking Retry/Resume should resume (handleClick)
+  const onRetryResumeClick = allCompleted
+    ? handleRetry
+    : handleClick;
+
+  return (
+    <>
+      <GlobalStyle />
+      <CompletionStatusCard className={className}>
+        <CompletionHeader>
+          {allCompleted
+            ? 'You are done.'
+            : (someCompleted ? 'Quiz is partially complete.' : 'No questions have been answered.')}
+        </CompletionHeader>
+
+        {handleRetry ? (
+          <div>
+            <p>{allCompleted ? unlimitedDone : unlimitetedCurrent}</p>
+            <ScoreGroup>
+              <p>
+                <b>Current Score:</b> {scoreSoFar ?? 'Score unavailable'} | <b>Saved Score:</b> {savedScore ?? 'Score unavailable'}
+              </p>
+            </ScoreGroup>
+          </div>
+        ) : (
+          <p>
+            {allCompleted
+              ? 'Great job answering all the questions.'
+              : (someCompleted
+                ? `You've completed ${numberCompleted} of ${numberOfQuestions} questions.`
+                : 'Begin working on the quiz.')}
+          </p>
+        )}
+
+        <ButtonGroup>
+          {handleRetry ? (
+            <RetryResumeButton
+              data-test-id="retry-resume-btn"
+              onClick={onRetryResumeClick}
+            >
+              {retryOrResume}
+            </RetryResumeButton>
+          ) : null}
+
+          <Button
+            data-test-id={`${buttonText.split(' ')[0].toLowerCase()}-btn`}
+            onClick={() => handleClick()}
+          >
+            {buttonText}
+          </Button>
+        </ButtonGroup>
+      </CompletionStatusCard>
+    </>
+  );
 })``;
