f5fa7c7a1b879d2f4cb5342632c912c757e5b3e2
Jose Carvajal <77166182+jomcarvajal@users.noreply.github.com>
2025-10-16 09:24:40 -0600

    CORE-1212: update styles for ProgressBar(open response questions) (#105)
    * update styles for ProgressBar(open response questions)
    
    * update jira-linked-action
    
    * typo

--- src/components/ProgressBar.stories.tsx
+++ src/components/ProgressBar.stories.tsx
@@ -1,23 +1,35 @@
 import { ProgressBar, ProgressBarItemVariant, ProgressBarProps } from './ProgressBar';
 
 const variants = new Array(8).fill({variant: 'isIncomplete'});
-const variantsInProgress: ProgressBarItemVariant[] = ['isIncorrect', 'isCorrect', 'isIncorrect', 'isIncorrect', 'isIncomplete', 'isIncomplete', 'isIncomplete', 'isIncomplete', 'isStatus'];
+const variantsInProgress: ProgressBarItemVariant[] = ['isIncorrect', 'isCorrect', 'isPartialCredit', 'isIncorrect', 'isIncomplete', 'isPartialCredit', 'isIncomplete', 'isIncomplete', 'isStatus'];
 
-const props: ProgressBarProps<{variant: ProgressBarItemVariant}> = {
+const props: ProgressBarProps<{variant: ProgressBarItemVariant; hasFeedback?: boolean}> = {
   activeIndex: 0,
   goToStep: (index: number, step) => console.log(index, step),
   steps: variantsInProgress.map((variant) => ({variant})),
 }
 
-const propsNoFeedback: ProgressBarProps<{variant: ProgressBarItemVariant}> = {
+const propsNoGraded: ProgressBarProps<{variant: ProgressBarItemVariant; hasFeedback?: boolean}> = {
   ...props,
   steps: variantsInProgress
-    .map(variant => variant === 'isCorrect' || variant === 'isIncorrect' ? null : variant)
-    .map(variant => ({ variant }))
+    .map(variant => variant === 'isCorrect' || variant === 'isIncorrect' || variant === 'isPartialCredit' ? null : variant)
+    .map(variant => ({ variant, hasFeedback: true }))
+}
+
+const propsHasFeedback: ProgressBarProps<{variant: ProgressBarItemVariant; hasFeedback?: boolean}> = {
+  ...props,
+  steps: variantsInProgress.map((variant) => ({variant, hasFeedback: true})),
+}
+
+const propsHasPartialFeedback: ProgressBarProps<{variant: ProgressBarItemVariant; hasFeedback?: boolean}> = {
+  ...props,
+  steps: variantsInProgress.map((variant, index) => ({variant, hasFeedback: index % 2 === 0 ? true : false})),
 }
 
 export const Default = () => <ProgressBar {...props} steps={[...variants, {variant: 'isStatus'}]} />;
 export const InProgress = () => <ProgressBar {...props} activeIndex={4} />;
 export const Review = () => <ProgressBar {...props} activeIndex={2} />;
 export const StatusStep = () => <ProgressBar {...props} activeIndex={8} />;
-export const InProgressNoFeedback = () => <ProgressBar {...propsNoFeedback} activeIndex={4} />;
+export const InProgressNoGraded = () => <ProgressBar {...propsNoGraded} activeIndex={4} />;
+export const InProgressHasFeedback = () => <ProgressBar {...propsHasFeedback} activeIndex={4} />
+export const InProgressHasPartialFeedback = () => <ProgressBar {...propsHasPartialFeedback} activeIndex={2} />
