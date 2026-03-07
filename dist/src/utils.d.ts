61d9963c0096e1a19f883f64aa6c07be6c1e6719
Prabhdip Gill <pg53@rice.edu>
2026-03-07 00:38:41 -0800

    dist

---
+++ dist/src/utils.d.ts
@@ -0,0 +1,8 @@
+import { Answer, ID } from '../src/types';
+export declare const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export declare const isAnswerCorrect: (answer: Answer, correctAnswerId?: ID | null) => boolean | undefined;
+export declare const isAnswerIncorrect: (answer: Answer, incorrectAnswerId?: ID) => boolean;
+export declare const isAnswerChecked: (answer: Answer, answerId?: ID) => boolean;
+export declare function countWords(text: string): number;
+export declare const numberfyId: (id: ID) => number;
+export declare const formatTimestamp: (timestamp: string | number) => string;
