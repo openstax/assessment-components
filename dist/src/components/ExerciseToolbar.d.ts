ccbe1585efe72d30dd8338ed28339eeb29c4357c
Prabhdip Gill <pg53@rice.edu>
2026-05-21 15:07:20 -0700

    dist

---
+++ dist/src/components/ExerciseToolbar.d.ts
@@ -0,0 +1,8 @@
+import { ExerciseIcons } from "./Exercise";
+export declare const StyledToolbar: import("styled-components").StyledComponent<"div", any, {
+    mobile: boolean;
+    desktop: boolean;
+}, never>;
+export declare const ExerciseToolbar: ({ icons }: {
+    icons?: ExerciseIcons | undefined;
+}) => JSX.Element | null;
