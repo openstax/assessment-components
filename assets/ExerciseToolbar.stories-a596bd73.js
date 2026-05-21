c39dbee2bf10722fbff018f36fa0c54d581fdb00
P-Gill97 <P-Gill97@users.noreply.github.com>
2026-05-21 22:59:42 +0000

    deploy: 0baf4de196530cf2143f506965e5da72865755ab

---
+++ assets/ExerciseToolbar.stories-a596bd73.js
@@ -0,0 +1,5 @@
+import{s as r,j as t}from"./index-89a6f642.js";import{E as o}from"./ExerciseToolbar-e9c4cdab.js";import"./theme-adad61a5.js";import"./index-37f797a5.js";const e=r.div`
+  overflow: hidden;
+  position: relative;
+  min-height: 20rem;
+`,l=()=>t(e,{children:t(o,{icons:{topic:{url:"https://openstax.org"},errata:{url:"https://openstax.org"}}})}),p=()=>t(e,{children:t(o,{icons:{topic:{url:"https://openstax.org",location:{toolbar:{desktop:!1,mobile:!0}}},errata:{url:"https://openstax.org",location:{toolbar:{desktop:!1,mobile:!0}}}}})});typeof window<"u"&&window.document&&window.document.createElement&&document.documentElement.setAttribute("data-storyloaded","");export{l as Default,p as DefaultMobileOnly};
